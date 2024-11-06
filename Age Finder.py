68e5196115dc8c6f77443dba8c2f3d05e64f1dfb
LeelaAI <leelabhaskarmondi@gmail.com>
2024-11-06 17:59:06 +0530

    Add files via upload

---
+++ Age Finder.py
@@ -0,0 +1,7 @@
+current_year =int(input("Enter your year current year:")) 
+birth_year = int(input("Enter your birth year: "))  # get input from the user and convert it to an integer
+
+age = current_year - birth_year  # calculate the age
+
+print("Your age is:", age)  # print the age
+print("program executed")
